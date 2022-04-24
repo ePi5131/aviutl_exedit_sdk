7dbecc9f1dadab5b55f78650fbfbc75178e33b9d
Kumrnm <84617660+kumrnm@users.noreply.github.com>
2022-04-24 13:36:26 +0900

    Improve implementation of variable reference

--- exedit/variables.hpp
+++ exedit/variables.hpp
@@ -6,159 +6,49 @@
 #include <exedit/Object.hpp>
 #include <exedit/CameraZBuffer.hpp>
 
-#define _OFFSET(exedit_version) static inline constexpr unsigned int offset##exedit_version
-
 namespace ExEdit {
 	namespace Variables {
 
-		struct staticFilterTable : Variable<Filter*, true> {
-			_OFFSET(092) = 0x0A3E28;
-			_OFFSET(093rc1) = 0x0C0A78;
-		};
-
-		struct aliasNameBuffer : Variable<char, true> {
-			_OFFSET(092) = 0x135C70;
-			_OFFSET(093rc1) = 0x18FDB8;
-		};
-
+		using staticFilterTable = Variable<Filter*, true, 0x0A3E28, 0x0C0A78>;
+		using aliasNameBuffer = Variable<char, true, 0x135C70, 0x18FDB8>;
 		struct objectDialog {
-			struct commandTarget : Variable<int> {
-				_OFFSET(092) = 0x14965C;
-				_OFFSET(093rc1) = 0x25E97C;
-			};
-
-			struct filterStatus : Variable<unsigned char, true> {
-				_OFFSET(092) = 0x158D38;
-				_OFFSET(093rc1) = 0x1C1310;
-			};
-
-			struct objectIndex : Variable<int> {
-				_OFFSET(092) = 0x177A10;
-				_OFFSET(093rc1) = 0x1051C8;
-			};
+			using commandTarget = Variable<int, false , 0x14965C, 0x25E97C>;
+			using filterStatus = Variable<unsigned char, true, 0x158D38, 0x1C1310>;
+			using objectIndex = Variable<int, false, 0x177A10, 0x1051C8>;
 		};
-
-		struct sceneSetting : Variable<SceneSetting, true> {
-			_OFFSET(092) = 0x177A50;
-			_OFFSET(093rc1) = 0x1051D0;
-		};
-
-		struct loadedFilterTable : Variable<Filter*, true> {
-			_OFFSET(092) = 0x187C98;
-			_OFFSET(093rc1) = 0x106490;
-		};
-
-		struct layerSetting : Variable<LayerSetting, true> {
-			_OFFSET(092) = 0x188498;
-			_OFFSET(093rc1) = 0x0FB580;
-		};
-
-		struct sceneDisplaying : Variable<int> {
-			_OFFSET(092) = 0x1A5310;
-			_OFFSET(093rc1) = 0x0EC098;
-		};
-
-		struct textBuffer : Variable<wchar_t, true> {
-			_OFFSET(092) = 0x1A6BD0;
-			_OFFSET(093rc1) = 0x1158B8;
-		};
-
+		using sceneSetting = Variable<SceneSetting, true, 0x177A50, 0x1051D0>;
+		using loadedFilterTable = Variable<Filter*, true, 0x187C98, 0x106490>;
+		using layerSetting = Variable<LayerSetting, true, 0x188498, 0x0FB580>;
+		using sceneDisplaying = Variable<int, false, 0x1A5310, 0x0EC098>;
+		using textBuffer = Variable<wchar_t, true, 0x1A6BD0, 0x1158B8>;
 		struct trackBarScript {
-			struct processingTrackBarIndex : Variable<int> {
-				_OFFSET(092) = 0x1B21F4;
-				_OFFSET(093rc1) = 0x1CD824;
-			};
-
-			struct processingObjectIndex : Variable<int> {
-				_OFFSET(092) = 0x1B2B04;
-				_OFFSET(093rc1) = 0x1CD59C;
-			};
+			using processingTrackBarIndex = Variable<int, false, 0x1B21F4, 0x1CD824>;
+			using processingObjectIndex = Variable<int, false, 0x1B2B04, 0x1CD59C>;
 		};
-
-		struct scriptProcessingFilter : Variable<Filter*> {
-			_OFFSET(092) = 0x1B2B10;
-			_OFFSET(093rc1) = 0x1CD6AC;
-		};
-
+		using scriptProcessingFilter = Variable<Filter*, false, 0x1B2B10, 0x1CD6AC>;
 #ifdef lua_h
-		struct luaState : Variable<lua_State, true> {
-			_OFFSET(092) = 0x1BACA8;
-			_OFFSET(093rc1) = 0x1CD560;
-		};
+		using luaState = Variable<lua_State, true, 0x1BACA8, 0x1CD560>;
 #endif
-
-		struct objectTable : Variable<Object[]> {
-			_OFFSET(092) = 0x1E0FA4;
-			_OFFSET(093rc1) = 0x12BA58;
-
-			struct size : Variable<int> {
-				_OFFSET(092) = 0x1E0FA0;
-				_OFFSET(093rc1) = 0x129A4C;
-			};
+		struct objectTable : Variable<Object[], false, 0x1E0FA4, 0x12BA58> {
+			using size = Variable<int, false, 0x1E0FA0, 0x129A4C>;
 		};
-
-		struct sortedObjectTable : Variable<Object*, true> {
-			_OFFSET(092) = 0x168FA8;
-			_OFFSET(093rc1) = 0x1B2560;
-
-			struct length : Variable<int> {
-				_OFFSET(092) = 0x146250;
-			};
-
-			struct layerIndexBegin : Variable<int> {
-				_OFFSET(092) = 0x149670;
-				_OFFSET(093rc1) = 0x1A0360;
-			};
-
-			struct layerIndexEnd : Variable<int> {
-				_OFFSET(092) = 0x135AC8;
-				_OFFSET(093rc1) = 0x1A0628;
-			};
+		struct sortedObjectTable : Variable<Object*, true, 0x168FA8, 0x1B2560> {
+			using length = Variable<int, false, 0x146250, NULL>;
+			using layerIndexBegin = Variable<int, false, 0x149670, 0x1A0360>;
+			using layerIndexEnd = Variable<int, false, 0x135AC8, 0x1A0628>;
 		};
-
-		struct objectExtraData : Variable<void*> {
-			_OFFSET(092) = 0x1E0FA8;
-			_OFFSET(093rc1) = 0x125A2C;
-
-			struct size : Variable<int> {
-				_OFFSET(092) = 0x1E0F9C;
-				_OFFSET(093rc1) = 0x12BA54;
-			};
+		struct objectExtraData : Variable<void*, false, 0x1E0FA8, 0x125A2C> {
+			using size = Variable<int, false, 0x1E0F9C, 0x12BA54>;
 		};
-
-		struct cameraZBuffer : Variable<ZBufferElm[]> {
-			_OFFSET(092) = 0x1EC7AC;
-			_OFFSET(093rc1) = 0x239658;
-		};
-
+		using cameraZBuffer = Variable<ZBufferElm[], false, 0x1EC7AC, 0x239658>;
 		struct undo {
-			struct buffer : Variable<void*> {
-				_OFFSET(092) = 0x244E0C;
-				_OFFSET(093rc1) = 0x248D00;
-
-				struct size : Variable<unsigned int> {
-					_OFFSET(092) = 0x244E18;
-					_OFFSET(093rc1) = 0x23A298;
-				};
-
-				struct objectNumber : Variable<int> {
-					_OFFSET(092) = 0x244E08;
-					_OFFSET(093rc1) = 0x13078C;
-				};
-
-				struct writeOffset : Variable<unsigned int> {
-					_OFFSET(092) = 0x244E10;
-					_OFFSET(093rc1) = 0x23A23C;
-				};
+			struct buffer : Variable<void*, false, 0x244E0C, 0x248D00> {
+				using size = Variable<unsigned int, false, 0x244E18, 0x23A298>;
+				using objectNumber = Variable<int, false, 0x244E08, 0x13078C>;
+				using writeOffset = Variable<unsigned int, false, 0x244E10, 0x23A23C>;
 			};
-
-			struct currentID : Variable<int> {
-				_OFFSET(092) = 0x244E14;
-				_OFFSET(093rc1) = 0x23A290;
-			};
+			using currentID = Variable<int, false, 0x244E14, 0x23A290>;
 		};
 
 	}
 }
-
-#undef _OFFSET
--- exedit/variables_core.hpp
+++ exedit/variables_core.hpp
@@ -3,27 +3,36 @@
 namespace ExEdit {
 	namespace Variables {
 
-		template <typename T, bool IsArray = false>
+		namespace __internal {
+			template <typename T>
+			struct variable_reference_type {
+				using type = T*;
+			};
+
+			template <typename T>
+			struct variable_reference_type<T[]> {
+				using type = T**;
+			};
+		}
+
+		template <typename T, bool isArray, unsigned int offset092, unsigned int offset093rc1>
 		struct Variable {
 			using type = T;
-			static inline constexpr bool isArray = IsArray;
-			using reference_type = T*;
-		};
-
-		template <typename T, bool IsArray>
-		struct Variable<T[], IsArray> {
-			using type = T[];
-			static inline constexpr bool isArray = IsArray;
-			using reference_type = T**;
+			static inline constexpr bool isArray = isArray;
+			static inline constexpr unsigned int offset092 = offset092;
+			static inline constexpr unsigned int offset093rc1 = offset093rc1;
+			using reference_type = __internal::variable_reference_type<T>::type;
 		};
 
 		template <typename Variable>
 		inline Variable::reference_type get092(const unsigned int exedit_base) {
+			static_assert(Variable::offset092 != NULL, "offset address for ExEdit version0.92 is unknown");
 			return reinterpret_cast<Variable::reference_type>(exedit_base + Variable::offset092);
 		}
 
 		template <typename Variable>
 		inline Variable::reference_type get093rc1(const unsigned int exedit_base) {
+			static_assert(Variable::offset093rc1 != NULL, "offset address for ExEdit version0.93rc1 is unknown");
 			return reinterpret_cast<Variable::reference_type>(exedit_base + Variable::offset093rc1);
 		}
 
