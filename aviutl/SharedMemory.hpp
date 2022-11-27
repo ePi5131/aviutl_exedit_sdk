fa630ef0fb36cb1cd866bca0ecff7f02bfe06a76
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-11-27 20:12:23 +0900

    Update SharedMemory.hpp

--- aviutl/SharedMemory.hpp
+++ aviutl/SharedMemory.hpp
@@ -2,12 +2,11 @@
 
 
 namespace AviUtl {
-    struct SharedMemoryInfo;
     struct SharedMemoryInfo {
-        HANDLE hFileMappingObject
+        HANDLE hFileMappingObject;
         void* mem_ptr;
         SIZE_T size;
-        SharedMemoryHandle** this_handle_ptr;
+        SharedMemoryInfo** this_handle_ptr;
         int key1;
         int key2;
         int id;
