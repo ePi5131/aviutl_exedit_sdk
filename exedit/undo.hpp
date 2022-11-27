4a404ee36f25b4ae873471aae48573bb684089ef
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-11-27 20:18:58 +0900

    Update undo.hpp

--- exedit/undo.hpp
+++ exedit/undo.hpp
@@ -8,8 +8,12 @@ namespace ExEdit {
 #pragma warning(disable: 4200)
     struct UndoData {
         int32_t data_id;
-        int32_t object_od;
+        int32_t object_id;
         int32_t data_size;
+        int32_t object_flag;
+        int32_t object_layer_disp;
+        int32_t object_frame_begin;
+        int32_t object_frame_end;
         std::byte data[];
     };
 #pragma warning(pop)
