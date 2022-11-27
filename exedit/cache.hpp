f92714c1b51013fcf28e0c913d7ff9dae9672bdd
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2022-11-27 14:25:43 +0900

    Update cache.hpp

--- exedit/cache.hpp
+++ exedit/cache.hpp
@@ -2,21 +2,18 @@
 #include <Windows.h>
 
 namespace ExEdit {
-    struct Cache {
-        uint32_t global_idx;
+    struct CacheDataInfo { // size = 276
         uint32_t w;
         uint32_t h;
         uint32_t bitcount;
         void* ptr;
         char name[260];
+    };
+    struct CacheInfo { // size = 9120
+        uint32_t priority;
+        CacheDataInfo main;
         uint32_t offset;
-        uint32_t local_num; // Piece count
-        struct Piece {
-            uint32_t w; // 0x120
-            uint32_t h;
-            uint32_t bitcount;
-            void* ptr; // maybe pointer
-            char name[260];
-        } piece[32];
+        uint32_t sub_num;
+        CacheDataInfo sub[32];
     };
 }
